f979155d3ea7aaa67416f0a27a77fdc151988e13
Max-Person <max_person@mail.ru>
2022-11-10 19:42:19 +0300

    [all] Первая попытка связть с++ и java сегменты

--- Lexer/main.cpp
+++ Lexer/main.cpp
@@ -14,19 +14,33 @@ topLevelDeclaration_node* root;
 
 extern void yyerror(char const* s);
 
+bool flagged(int argc, char** argv, const char* flag) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], flag) == 0) return true;
+    }
+    return false;
+}
+
 int main(int argc, char** argv) {
     if (argc > 1) {
         yyin = fopen(argv[1], "r");
-        if (argc > 2 && strcmp(argv[2], "-d")==0) yydebug = 1;
+        if (flagged(argc, argv, "-d")) yydebug = 1;
         int res = yyparse();
         if(res == 0){
             dotOut::displayInit(root);
             xmlOut::displayInit(root);
-            printf("Successful\n");
+            printf("C++ Segment (lexer + parser): SUCCESS!\n");
         }
+        else fprintf(stderr, "C++ Segment (lexer + parser) ERR: parse FAILED;\n");
         fclose(yyin);
+
+        if (!flagged(argc, argv, "--no-sem")) {
+            //string command = "java -cp \"..\\java semantic analysis\\out\\production\\java semantic analysis\" Main xmlOutput.xml";
+            string command = "java -jar \"java semantic analysis.jar\" xmlOutput.xml";
+            system(command.c_str());
+        }
     }
     else {
-        yyerror("Not found file");
+        fprintf(stderr, "C++ Segment (lexer + parser) ERR: FILE NOT FOUND;\n");
     }
 }
--- java semantic analysis/src/Main.java
+++ java semantic analysis/src/Main.java
@@ -8,14 +8,32 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-
-        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File("S:\\engRoute\\Dart Compiler\\Debug\\xmlOutput.xml")); // путь к файлу
-        document.normalize();
-    
-        RootNode root = new RootNode(document.getDocumentElement());
+    public static void main(String[] args){
+        if(args.length == 0){
+            System.err.println("Java Segment (semantic) ERR: provide path to parser's xmlOutput file.");
+            return;
+        }
+        
+        Document document = null;
+        try {
+            document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(args[0]));
+            document.normalize();
+        } catch (Exception e) {
+            System.err.println("Java Segment (semantic) ERR: " + e.getLocalizedMessage().toUpperCase());
+            return;
+        }
+        
+        try{
+            RootNode root = new RootNode(document.getDocumentElement());
+        } catch (Exception e) {
+            System.err.println("Java Segment (semantic) ERR: " + e.getLocalizedMessage().toUpperCase());
+            return;
+        }
+        
+        System.out.println("Java Segment (semantic): tree building SUCCESS!");
 
     }
 }
